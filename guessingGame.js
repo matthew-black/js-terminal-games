7179f8373ac5713a6e8285c6844fdabdd931e2d8
Matthew Black <matthewallenblack@gmail.com>
2023-04-19 09:10:16 -0500

    develop incredible game

---
+++ guessingGame.js
@@ -0,0 +1,39 @@
+const prompt = require('prompt-sync')();
+
+let numberOfGuesses = 0;
+let hasGuessedCorrectly = false;
+
+while (hasGuessedCorrectly != true) {
+  clearScreen();
+  
+  console.log(`You've made ${numberOfGuesses} guesses!`)
+
+  if (numberOfGuesses > 3) {
+    console.log('\tHint time! 🐬!')
+  }
+  
+  const guess = prompt('What word do you think it is? ');
+  numberOfGuesses++;  
+
+  if (guess === 'dolphin') {
+    hasGuessedCorrectly = true;
+  }
+}
+
+clearScreen();
+console.log('Nice work! It was totally dolphin!');
+console.log(`And! It only took you ${numberOfGuesses} guesses!`);
+
+
+function clearScreen() {
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+  console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
+}
